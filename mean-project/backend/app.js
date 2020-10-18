44434cb4a6df245f69dc9d142e4cb671cdde0fe8
ccodecheff <rathore.karan.sn1@gmail.com>
2020-10-17 17:02:14 -0700

    updating server side routings

--- mean-project/backend/app.js
+++ mean-project/backend/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose= require("mongoose");
 
-const Post= require('./models/post');
+
+const postsRoutes= require('./routes/posts')
+
 
 const app = express();
 // Databasename: node-angular
@@ -34,45 +36,5 @@ mongoose.connect("mongodb+srv://karan:8p5Uk2VmzeiHzMMx@cluster0.swcbv.mongodb.ne
     next();
   });
 
-  app.post("/api/posts", (req, res, next) => {
-    const post = new Post({
-      title: req.body.title,
-      content: req.body.content
-    });
-    post.save().then(createdPost => {
-      res.status(201).json({
-        message: "Post added successfully",
-        postId: createdPost._id
-      });
-    });
-  });
-
-  app.put("/api/posts/:id", (req, res, next) => {
-    const post = new Post({
-      _id: req.body.id,
-      title: req.body.title,
-      content: req.body.content
-    });
-    Post.updateOne({ _id: req.params.id }, post).then(result => {
-      console.log(result);
-      res.status(200).json({ message: "Update successful!" });
-    });
-  });
-
-  app.get("/api/posts", (req, res, next) => {
-    Post.find().then(documents => {
-      res.status(200).json({
-        message: "Posts fetched successfully!",
-        posts: documents
-      });
-    });
-  });
-
-  app.delete("/api/posts/:id", (req, res, next) => {
-    Post.deleteOne({ _id: req.params.id }).then(result => {
-      console.log(result);
-      res.status(200).json({ message: "Post deleted!" });
-    });
-  });
-
-  module.exports = app;
+    app.use("/api/posts",postsRoutes);
+    module.exports = app;
---
+++ mean-project/backend/routes/posts.js
@@ -0,0 +1,49 @@
+const express= require('express');
+const Post= require('../models/post');
+
+
+const router = express.Router();
+
+
+router.post("", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Post added successfully",
+      postId: createdPost._id
+    });
+  });
+});
+
+router.put("/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({ _id: req.params.id }, post).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Update successful!" });
+  });
+});
+
+router.get("", (req, res, next) => {
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents
+    });
+  });
+});
+
+router.delete("/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Post deleted!" });
+  });
+});
+
+module.exports= router;
