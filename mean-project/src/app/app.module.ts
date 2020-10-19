1d9e1402dd6d045c14657366eadf683834d7d3be
ccodecheff <rathore.karan.sn1@gmail.com>
2020-10-18 19:23:06 -0700

    added pagination to project

--- mean-project/src/app/app.module.ts
+++ mean-project/src/app/app.module.ts
@@ -9,6 +9,7 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatExpansionModule} from "@angular/material/expansion";
 import{MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatPaginatorModule} from "@angular/material/paginator";
 
 
 import { AppComponent } from "./app.component";
@@ -37,7 +38,8 @@ import{AppRoutingModule} from './app-routing.module';
     MatToolbarModule,
     MatExpansionModule,
     HttpClientModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatPaginatorModule
   ],
   providers: [PostsService],
   bootstrap: [AppComponent]
